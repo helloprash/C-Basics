4115146872356f89817f27f33aa105410fe04bf9
vlsiguruemb <31706933+vlsiguruemb@users.noreply.github.com>
2017-09-19 11:03:02 +0530

    Create strTk.c

---
+++ whiteboard/19-Sep-2017/strTk.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+int strTok(char *input, char delimiter,char *token)
+{
+   /* if the function has hit the end of string it wil
+      return ZERO otherwise 1 */
+   static char *buffer ;
+   static int  position ;
+   int idx ;
+   int flag ;
+   if (input != NULL)
+   {
+       buffer = input ;
+       position = 0 ;
+       flag = 1 ;
+       /* Search for the delimiter and return */
+       while(flag)
+       {
+          if buffer[position] == delimiter)
+          {
+              /* We had found the token */
+              for (idx = 0 ; idx < position;idx++)
+              {
+                 token[idx] = buffer[idx];
+              }
+              token[idx] = '\0';
+              return 1 ;
+          }
+          if (buffer[position] == NULL)
+          {
+             return 0;
+          }
+
+          position++ ;
+       }
+
+   }
+   else
+   {
+      /* Repeated call */
+      if (buffer == NULL)
+      {
+        token = NULL ;
+         return 0 ;
+      }
+      /* Search for the delimiter and return */
+   }
+
+
+}
+
+main()
+{
+}
